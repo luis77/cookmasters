a4bb5e2f0008a5462da36aa075bbbc8c3484762b
luis <luisluislu31@gmail.com>
2019-06-10 04:26:49 -0400

    div con un alto minimo adaptable

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-  has_attached_file :foto, styles: {thumb: "640x426" }
+  has_attached_file :foto, styles: {recortada: "640x426#", thumb: "640x426" }
   validates_attachment_content_type :foto, content_type: /\Aimage\/.*\z/
   belongs_to :user
   has_many :ingredients, dependent: :destroy
